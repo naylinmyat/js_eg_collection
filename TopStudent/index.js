2c65bc17d184cfa67801c74c5769e5d48ac1eba0
macAir <saiyemyataung@Yins-MacBook.local>
2022-06-22 20:09:22 +0630

    Top-Student

---
+++ TopStudent/index.js
@@ -0,0 +1,41 @@
+let students = [
+    {
+      name: "David Rayy",
+      mark: 12,
+    },
+    {
+      name: "NYAN Rayy",
+      mark: 1,
+    },
+    {
+      name: "KO Rayy",
+      mark: 2,
+    },
+    {
+      name: "LIN Rayy",
+      mark: 40,
+    },
+    {
+      name: "TIN Rayy",
+      mark: 90,
+    },
+    {
+      name: "DEOK Rayy",
+      mark: 1,
+    },
+    {
+      name: "KOOO Rayy",
+      mark: 100,
+    },
+];
+function findTopStudentName(arr){
+    let topStudent = students[0];
+    for(let i = 1; i < arr.length; i++){
+        if(students[i].mark > topStudent.mark){
+            topStudent = students[i];
+        }
+    }
+    return topStudent.name;
+}
+let result = findTopStudentName(students);
+console.log(result);
