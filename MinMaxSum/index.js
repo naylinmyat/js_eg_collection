eff1a63b896c1f93a1c33fdf6ff1fe3af347992c
macAir <saiyemyataung@Yins-MacBook.local>
2022-06-23 14:49:03 +0630

    Min-Max-Sum

--- MinMaxSum/index.js
+++ MinMaxSum/index.js
@@ -1,16 +1,16 @@
-let sampleArray = [1,3,5,7,9];
+let sampleArray = [1,2,3,4,5];
 
 function miniMaxSum(arr){
     let maxNumber = -1;
     let miniNumber = Number.MAX_SAFE_INTEGER;
     let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        sum += arr[i];
-        if(arr[i] > maxNumber){
-            maxNumber = arr[i];
+    for(let value of arr){
+        sum += value;
+        if(value > maxNumber){
+            maxNumber = value;
         }
-        if(arr[i] < miniNumber){
-            miniNumber = arr[i];
+        if(value < miniNumber){
+            miniNumber = value;
         }
     }
     let miniSum = sum - maxNumber;
